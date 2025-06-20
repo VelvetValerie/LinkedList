5dc3bd430a1ef5a48c6f18da5b6718ead2ee74b0
Velvet <rafifadillah420@gmail.com>
2025-06-20 13:41:34 +0800

    Menambahkan fungsi tambahbarang

--- Menu.cpp
+++ Menu.cpp
@@ -2,20 +2,43 @@
 #include <string>
 using namespace std;
 
+//Struktur node barang
 struct Barang {
     string nama;
     int jumlah;
     Barang* next;
 };
 
+//Fungsi untuk menambahkan barang
+void tambahBarang(Barang*& head) {
+    Barang* baru = new Barang();
+    cout << "Masukkan nama buah: ";
+    cin.ignore();
+    getline(cin, baru->nama);
+    cout << "Masukkan jumlah: ";
+    cin >> baru->jumlah;
+    baru->next = nullptr;
+
+     if (head == nullptr) {
+        head = baru;
+    } else {
+        Barang* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = baru;
+    }
+    cout << "Buah berhasil ditambahkan!\n";
+}
+
 int main() {
     int pilihan;
     do {
         cout << "=== Menu Inventori ===" << endl;
-        cout << "1. Tambah Barang" << endl;
-        cout << "2. Daftar Barang" << endl;
-        cout << "3. Cari Barang" << endl;
-        cout << "4. Hapus Barang" << endl;
+        cout << "1. Tambah Buah" << endl;
+        cout << "2. Daftar Buah" << endl;
+        cout << "3. Cari Buah" << endl;
+        cout << "4. Hapus Buah" << endl;
         cout << "0. Keluar" << endl;
         cout << "Pilih menu: ";
         cin >> pilihan;

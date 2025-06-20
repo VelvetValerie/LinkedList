e408a2d183f9fe372b65eed888975f842c3fe965
Velvet <rafifadillah420@gmail.com>
2025-06-20 13:27:11 +0800

    Menambahkan fungsi menu

---
+++ Menu.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int pilihan;
+    do {
+        cout << "=== Menu Inventori ===" << endl;
+        cout << "1. Tambah Barang" << endl;
+        cout << "2. Daftar Barang" << endl;
+        cout << "3. Cari Barang" << endl;
+        cout << "4. Hapus Barang" << endl;
+        cout << "0. Keluar" << endl;
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+
+        switch(pilihan) {
+            case 1:
+                cout << "Fitur Tambah Barang\n";
+                // Tambahkan kode tambah barang di sini
+                break;
+            case 2:
+                cout << "Fitur Daftar Barang\n";
+                // Tambahkan kode daftar barang di sini
+                break;
+            case 3:
+                cout << "Fitur Cari Barang\n";
+                // Tambahkan kode cari barang di sini
+                break;
+            case 4:
+                cout << "Fitur Hapus Barang\n";
+                // Tambahkan kode hapus barang di sini
+                break;
+            case 0:
+                cout << "Keluar dari program.\n";
+                break;
+            default:
+                cout << "Pilihan tidak valid.\n";
+        }
+        cout << endl;
+    } while(pilihan != 0);
+
+    return 0;
+}
